62d6aad3629adf3873091d255c9d8e3f3ebf59fc
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2026-08-08 10:36:55 +0200

    #gg: Finish development of version 1.0.1

--- lib/src/dna_dart_version.dart
+++ lib/src/dna_dart_version.dart
@@ -1,5 +1,5 @@
 // @license
-// Copyright (c) 2026 ggsuite
+// Copyright (c) ggsuite
 //
 // Use of this source code is governed by terms that can be
 // found in the LICENSE file in the root of this package.
@@ -9,4 +9,4 @@
 // coverage:ignore-file
 
 /// The version of the `dna_dart` package.
-const String dnaDartVersion = '1.0.0';
+const String dnaDartVersion = '1.0.1';
